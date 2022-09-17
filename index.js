a3d48f77eca005264058d8ec301ac5285d929794
barimess <mbari99@hotmail.fr>
2022-09-17 23:39:17 +0100

    added comments to clear confusion.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let ratingstate = "";
 
+// function to select a rating and clear other selections
 function select(obj) {
  ratingstate = obj.innerHTML;
  let wiper ;
+ //scuffed way of clearing selections.
  for(let i=1; i<6; i++) {
    wiper = document.getElementById(`number-${i}`);
    wiper.classList.remove("selected");
  }
  obj.classList.add("selected");
 }
+
+// TODO: find a way to translate functionality towards the thank you wrapper.
