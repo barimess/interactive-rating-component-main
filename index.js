13f84d15df60b38f4fcdb53590eb214d8db6417f
barimess <mbari99@hotmail.fr>
2022-09-19 00:34:50 +0100

    submit section is now functional. added fadeout and fade in animations.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let ratingstate = "";
+let rating_state;
 
 // function to select a rating and clear other selections
 function select(obj) {
- ratingstate = obj.innerHTML;
+ rating_state = obj.innerHTML;
  let wiper ;
  //scuffed way of clearing selections.
  for(let i=1; i<6; i++) {
@@ -12,4 +12,44 @@ function select(obj) {
  obj.classList.add("selected");
 }
 
-// TODO: find a way to translate functionality towards the thank you wrapper.
+//function to submit the rating to the thank you form.
+function submitRating(){
+  // get rating from previous function and change the html afterwards
+  let rating_value = document.getElementById("rating-value");
+  if (!rating_state) {return};
+  rating_value.innerHTML = rating_state;
+
+  //fade out animation 
+  let rating_section = document.getElementById("rating-section");
+  fadeOut(rating_section);
+
+  //fade in animation
+  let thankyou_section = document.getElementById("thankyou-section");
+  fadeIn(thankyou_section, rating_section);
+  }
+
+function fadeOut(obj) {
+  obj.classList.add("animate-out");
+  obj.addEventListener('animationend', function(e) {
+    obj.classList.add("hidden");
+  }, {
+    capture: false,
+    once: true,
+    passive: false
+  });
+}
+
+function fadeIn(obj, obj2) {
+  
+
+  obj2.addEventListener('animationend', function(e) {
+    obj.classList.remove("hidden");
+    obj.classList.add("animate-in");
+    obj.classList.remove("visuallyhidden")
+  }, {
+    capture: false,
+    once: true,
+    passive: false
+  });
+  
+}
